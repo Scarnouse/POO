173c0fac7c83b29dc7b78eb27fca842739bef242
scarn <scarn@DESKTOP-C2K3O10>
2016-02-22 21:19:53 +0100

    ...

--- src/Ejercicios/BancoNuevo.java
+++ src/Ejercicios/BancoNuevo.java
@@ -5,11 +5,12 @@ import java.util.Random;
 public class BancoNuevo {
 	Banco b = new Banco();
 	Random r = new Random();
+	String[] arrayCuentas = new String[5];
 	String cuentaAleatoria = "";
 	for(int j=0; i<5; i++){
 		for(int i=0; i<10; i++) {
 			cuentaAleatoria += r.nextInt(10);
 		}
-		b.guardarCuentas(cuentaAleatoria);
-	}
+		arrayCuentas[j] = cuentaAleatoria;
+	}	
 }
